77867ffe9327ab7e37fee3b565c792891afce676
MLechiakh <mohamed.mp3m@gmail.com>
2020-02-03 10:04:35 -0500

    update repo for for-loop implementation

--- master_test.c
+++
@@ -1,8 +0,0 @@
-int main(int argc, char** argv) {
-  uint64_t i;
-  uint64_t sum;
-
-  sum = 0;
-
-  return sum + 39;
-}
--- test.c
+++
@@ -1,9 +0,0 @@
-int main(int argc, char** argv) {
-
-uint64_t  i ;
-for(i=0 ;i<17;i=i+2){
-
-
-}
-return i;
-}
